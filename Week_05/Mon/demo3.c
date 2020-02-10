944864877f9696812458a2d1e87d4a6598136167
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-02-10 15:49:33 -0700

    Added status check

--- Week_05/Mon/demo3.c
+++ Week_05/Mon/demo3.c
@@ -24,17 +24,29 @@ void main(int argc, char** argv)
         for(int p = 1; p < nprocs; p++)
         {
             int dest = p;
-            double x = random_number();
-            printf("Sending %f to rank %d\n",x,&p);
+            double x[3];
+            x[0] = random_number();
+            x[1] = random_number();
+            x[2] = random_number();
+            printf("Sending %f to rank %d\n",x[0],p);
             MPI_Send(x,3,MPI_DOUBLE,dest,tag,MPI_COMM_WORLD);
         }
     }
     else
     {
         int sender = 0;
-        double x;
-        MPI_Recv(x,1,MPI_DOUBLE,sender,tag,MPI_COMM_WORLD,&status);
-        printf("Rank %d received %f\n",my_rank,*x);
+        double x[3];
+        MPI_Recv(x,2,MPI_DOUBLE,sender,tag,MPI_COMM_WORLD,&status);
+#if 1        
+        int count;
+        int ierror = MPI_Get_count(&status,MPI_DOUBLE,&count);
+        if (count != 3)
+        {
+            printf("ierror = %d; Did not receive all info\n",ierror);
+            exit(0);
+        }
+#endif        
+        printf("Rank %d received %f\n",my_rank,x[0]);
     }
 
     MPI_Finalize();
