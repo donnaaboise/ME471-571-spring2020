30698280fc176172601dca41a7fd11b284ffe0dc
Donna Calhoun <donnacalhoun@boisestate.edu>
2020-04-08 16:24:49 -0600

    Update header file

--- homework/hmwk4/cg_serial.h
+++ homework/hmwk4/cg_serial.h
@@ -1,10 +1,27 @@
 #ifndef CG_SERIAL_H
 #define CG_SERIAL_H
 
+
+#ifdef __cplusplus
+extern "C"
+{
+#if 0
+}
+#endif
+#endif
+
+
 typedef void (*matmult_t)(int N, double* x,double *Ax);
 
 void cg_1d(int N, int mbc, int kmax, double tol, int prt, matmult_t matmult, 
         double *x, double *F, int* it_cnt);
+
+#ifdef __cplusplus
+#if 0
+{
+#endif
+}
+#endif
 
 
 #endif
